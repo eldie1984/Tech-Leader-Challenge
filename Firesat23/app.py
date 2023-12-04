d04dc3d0eb5e685454890753277b69f1b2bfd625
Diego Gasch <eldie1984@gmail.com>
2023-12-04 09:41:25 -0300

    Update code

--- Firesat23/app.py
+++ Firesat23/app.py
@@ -6,6 +6,8 @@ from moto import mock_dynamodb
 from database.init import init_db
 from api.firesat23 import get_wildfires_last_24_hours
 
+from utils.convert_data import generate_row_database,row_to_dict,insert_batch,read_table
+
 @mock_dynamodb
 def lambda_handler (event: dict, context: dict) -> dict:
 
@@ -13,14 +15,23 @@ def lambda_handler (event: dict, context: dict) -> dict:
   firesat_csv: str = get_wildfires_last_24_hours()
   uploadFrom = datetime.strptime(event["uploadFrom"], "%Y-%m-%dT%H:%M:%S+00:00")
   firstLine: bool = True
+  items=[]
 
   for row in firesat_csv.splitlines():
     # Extraer la información, detectar continente y subir a la base de datos
     # Se recomienda usar la función "get_continent" del archivo "utils/get_continent.py"
     # Modularizar la solucion para hacerla más clara y entendible
-    firstLine = False
-
+    if firstLine:
+        header=row.split(',')
+        firstLine = False
+    else:
+        row_dic=row_to_dict(row.split(','),header)
+        if row_dic['continent']!='UNKNOWN' and datetime.strptime(row_dic['date'], "%Y-%m-%dT%H:%M:%S+00:00")>uploadFrom:
+            items.append(generate_row_database(row_dic))
+  insert_batch(items)
   # Escribir el codigo para leer toda la base de datos y outputearla.
   # Esto sera util para testear el codigo
-  wholeDatabase: dict = {}
+  wholeDatabase: dict = read_table()
   return { "statusCode": 200, "body": wholeDatabase}
+
+
--- Firesat23/utils/convert_data.py
+++ Firesat23/utils/convert_data.py
@@ -1,28 +1,14 @@
-import pandas as pd
+import boto3
 from decimal import Decimal
 
 from utils.get_continent import get_continent
-#function that recieve an array of strings and returns an array of arrays
-def convert_data(data):
-    new_data = []
-    for d in data:
-        new_data.append(d.split(','))
-    return new_data
-
-def convert_dataframe(array, date):
-    df=pd.DataFrame(array[1:],columns=array[0])
-    df['x']=pd.to_numeric(df['x'])
-    df['y']=pd.to_numeric(df['y'])
-    df['date']=pd.to_datetime(df['date'])
-    df['continent']=df.apply(lambda row: get_continent(row['x'], row['y']), axis=1)
-    return df[df['date']>=date]
 
 
 def generate_row_database(row: dict)->list:
     return {
     "continent_date": "{}_{}".format(row['continent'],row['date'][:14]), # Es el continente en dos caracteres + fecha con formato ISO 8601 cortada hasta la hora
     "id": "{}+{}+{}".format(row['date'],round(float(row['x']),2),round(float(row['y']),2)), # Es el ISO 8601 + latitud + longitud con aproximación de dos dígitos
-    "conf": 15, # Es la confianza de la detección
+    "conf": row['conf'], # Es la confianza de la detección
     "sat": "noaa-goes16",
     "x": Decimal(row['x']),
     "y": Decimal(row['y'])
@@ -34,3 +20,24 @@ def row_to_dict(row:list,header:list)->dict:
         a_dict[header[i]]=row[i]
     a_dict['continent']=get_continent(a_dict['x'], a_dict['y'])
     return(a_dict)
+
+def insert_batch(items:list) -> None:
+
+  dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+  table_name = 'WildfiresTable'
+
+  table = dynamodb.Table(table_name)
+  with table.batch_writer() as batch:
+    for item in items:
+        batch.put_item(Item=item)
+
+def read_table():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table_name = 'WildfiresTable'
+    table = dynamodb.Table(table_name)
+    response = table.scan()
+    items = response['Items']
+
+    for item in items:
+        print(item)
+    return items
